a48aaccb349eedc073fb671f86b144f3d1a1047f
Maicon Borba <maiconbsconceicao@gmail.com>
2025-08-29 19:20:05 -0300

    new environment

--- src/app/concessionaria/create/page.jsx
+++ src/app/concessionaria/create/page.jsx
@@ -129,7 +129,7 @@ export default function ConcessionariaCreate() {
     const formData = new FormData();
     formData.append('image', imageFile);
     
-    const response = await fetch(`https://api.imgbb.com/1/upload?key=${process.env.NEXT_APP_IMGBB_API_KEY}`, {
+    const response = await fetch(`https://api.imgbb.com/1/upload?key=${process.env.NEXT_PUBLIC_IMGBB_API_KEY}`, {
         method: 'POST',
         body: formData
     });
